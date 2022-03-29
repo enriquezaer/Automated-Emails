38a0d5c07cc190e053fd69952d2c9016a6b23959
alex <enriquezaer@gmail.com>
2022-03-29 06:02:23 -0600

    creation of sending cicle.

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import yagmail
 import pandas
+import datetime
+import time
 from news import NewFee
 
 #df = pandas.read_csv("testFile.csv")
 
 df = pandas.read_excel('people.xlsx')
-for index, row in df.iterrows():
-    news_feed = NewFee(interest=row['interest'], from_date='2022-03-20', to_date='2022-03-23')
+while True:
+    if datetime.datetime.now().hour == 5 and datetime.datetime.now().minute == 59:
+        for index, row in df.iterrows():
+            today = datetime.datetime.now().strftime("%Y-%m-%d")
+            yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d)")
+            news_feed = NewFee(interest=row['interest'],
+                               from_date=yesterday,
+                               to_date=today)
 
-    email = yagmail.SMTP(user="enriquezaer@gmail.com", password="G@mbit@01")
-    email.send(to="nuhudesa@thichanthit.com", subject=f"Hola tus noticias sobre {row['interest']} para hoy",
-               contents=f"Hola {row['name']}!  mira lo que hay sobre  {row['interest']} el dia de hoy.\n {news_feed.get()}\nAlex")
-
+            email = yagmail.SMTP(user="enriquezaer@gmail.com", password="G@mbit@01")
+            email.send(to="waxibip611@jooffy.com", subject=f"Hola tus noticias sobre {row['interest']} para hoy",
+                       contents=f"Hola {row['name']}!  mira lo que hay sobre  {row['interest']} el dia de hoy.\n {news_feed.get()}\nAlex")
+    time.sleep(60)
